4f6d39be16a09424cfbbe58fc69380a464e12e03
Masao Kitamura <masaok@gmail.com>
2021-06-14 17:55:13 -0700

    Starting demo stuff framework from react-scrolling-demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+import { Helmet } from 'react-helmet'
+
+const App = (props: any) => {
+  const classes = useStyles(props)
+
+  const [name] = useState('MASAO')
+
+  console.log('HEYYYYY')
+
+  // const doStuff = () => {
+  //   console.log('DOING STUFF')
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Helmet>
+        <title>React Scrolling Demo - {name}</title>
+      </Helmet>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <div className={classes.root}>
+            <Switch>
+              <Route path="/" component={Header} />
+            </Switch>
+
+            <main className={classes.content}>
+              <Switch>
+                <Route exact path="/" component={ScrollInsideContainer} />
+                <Route path="/scroll-inside-container" component={ScrollInsideContainer} />
+                <Route path="/paper-scroll-demo" component={PaperScrollDemo} />
+                <Route path="/paper-grid-scroll-demo" component={PaperGridScrollDemo} />
+                <Route path="/paper-grid-scrollbar-demo" component={PaperGridScrollBarDemo} />
+                <Route path="/state-variable-copy-demo" component={StateVariableCopyDemo} />
+              </Switch>
+            </main>
+
+            <Switch>
+              <Route path="/" component={Footer} />
+            </Switch>
+          </div>
+        </Router>
+      </ThemeProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
